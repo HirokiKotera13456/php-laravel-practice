de98a56864ceeec1e81ec9697a8b82fe2a3aba4c
nijimajohn <hirokikotera0211@gmail.com>
2021-11-02 20:01:37 +0900

    [追加]ダミーデータをviewファイルに追加

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Blog;
 
 class BlogController extends Controller
 {
@@ -11,10 +12,11 @@ class BlogController extends Controller
      * @return view
      */
 
-
     public function showList()
     {
-        return view('blog.list');
+        $blogs = Blog::all();
+
+        return view('blog.list',['blogs' => $blogs]);
     }
 
 }
