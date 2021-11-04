ad8a5dcb5273ddbe8d9ff167319b2fb24124c1f8
nijimajohn <hirokikotera0211@gmail.com>
2021-11-04 15:22:41 +0900

    [追加]削除機能

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,9 @@ Route::post('/blog/store', 'BlogController@exeStore')->name('store');
 //ブログ詳細画面を表示
 Route::get('/blog/{id}','BlogController@showDetail')->name('show');
 
-
 // ブログ編集画面を表示
 Route::get('/blog/edit/{id}', 'BlogController@showEdit')->name('edit');
 Route::post('/blog/update', 'BlogController@exeUpdate')->name('update');
+
+//　ブログ削除
+Route::post('/blog/delete/{id}', 'BlogController@exeDelete')->name('delete');
